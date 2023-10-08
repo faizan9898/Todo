74c41376640f3a956a478c7d582cce0e50fc6866
faizan arif <113965832+faizan9898@users.noreply.github.com>
2023-10-08 16:22:48 +0530

    Add files via upload
    A Python and Django-based to-do project using a SQLite database is a web application that allows users to create, manage, and organize their tasks and to-do lists efficiently. It provides a user-friendly interface for adding, editing, and deleting tasks, making it a convenient tool for personal task management.

---
+++ todo/todonew/models.py
@@ -0,0 +1,14 @@
+
+
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Todo(models.Model):
+	title = models.CharField(max_length=100)
+	details = models.TextField()
+	date = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.title
